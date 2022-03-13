eceb8b05f0b4e54dcec2b70c03fcd540dc13aee7
AbigailMezaC <mezacruzabigail@gmail.com>
2022-03-13 16:18:04 -0600

    js Ejercicios

--- 09EjerciciosJS/js/Ejercicio1.js
+++ 09EjerciciosJS/js/Ejercicio1.js
@@ -1,1 +1,27 @@
-jsj
+function validar(e){
+    var teclado = (document.all)?e.KeyCode : e.which
+
+    if(teclado==8) return true;
+    var patron = /[0-9\d .]/;
+
+    var probar = String.fromCharCode(teclado);
+
+    return patron.test(probar);
+}
+
+function ganancia(){
+    var dinero = document.formulario1.cantidad.value;
+    
+    var resultado = parseInt(dinero);
+
+    var porcentaje = resultado*0.02;
+
+    var total = resultado + porcentaje;
+
+    document.formulario1.intMen.value = "$" + total;
+}
+
+function borrar(){
+    document.formulario1.intMen.value = "";
+    document.formulario1.cantidad.value = "";
+}
